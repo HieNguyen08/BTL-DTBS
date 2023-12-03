2ed0aaab8789d2d9a4303c066254a89a27f09f3c
TFPAlive <71390622+TFPAlive@users.noreply.github.com>
2023-12-03 16:14:36 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -5,7 +5,19 @@ if (isset($_SESSION['id']) && isset($_SESSION['user'])) {
     if (isset($_GET["id"])) {
         $id = $_GET["id"];
 
+        $sql = "DELETE FROM books WHERE Document_ID=$id";
+        $conn->query($sql);
+        $sql = "DELETE FROM book_author WHERE Document_ID=$id";
+        $conn->query($sql);
         $sql = "DELETE FROM documents WHERE Document_ID=$id";
+        $conn->query($sql);
+        $sql = "DELETE FROM documents_language WHERE Document_ID=$id";
+        $conn->query($sql);
+        $sql = "DELETE FROM review_comments WHERE Document_ID=$id";
+        $conn->query($sql);
+        $sql = "DELETE FROM published_by WHERE Document_ID=$id";
+        $conn->query($sql);
+        $sql = "DELETE FROM publishers WHERE Document_ID=(SELECT Document_ID published_by WHERE Document_ID=$id)";
         $conn->query($sql);
         header("location:home.php");
         exit();
